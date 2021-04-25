e4903b98d1d12d003c0bf5ed4ccbcb353f38f9de
Naotchi <n.shiba0101@gmail.com>
2021-04-25 13:36:21 +0900

    before using class

--- mecanum_motor/src/main.cpp
+++ mecanum_motor/src/main.cpp
@@ -5,15 +5,14 @@
 #include "main.h"
 #include "mbedserial.h"
 
-#define SUMPLING_TIME_US 100000 //タイマー割り込み周期 kHzオーダーつまり1000が理想
-#define PERIOD 1000 //モーターのMAX値
-const double resolution = 10;  // エンコーダーの分解能
-const double r = 10;  // タイヤの半径 / m
+#define SUMPLING_TIME_US 1000  // タイマー割り込み周期 kHzオーダーつまり1000が理想
+#define COMM_TIME_US 10000
+#define PERIOD 1000  // PWM制御のパルス幅 μs
 
 Serial pc(USBTX, USBRX, 115200);
 Mbedserial Ms(pc);
 InterruptIn switch1(USER_BUTTON);
-Ticker ticker1, ticker2, ticker3, ticker4;
+Ticker ticker1, ticker2;
 
 //モーター
 PwmOut PWM(PA_8);
@@ -24,10 +23,13 @@ Motor motor(PWM, PHASE, PERIOD, true);
 InterruptIn enA(PA_9);
 DigitalIn enB(PB_4);
 
+double resolution = 10;  // エンコーダーの分解能
+double r = 0.1;  // タイヤの半径 m
+
 // モーターの速度
-double target_speed;  // 目標値
-double current_speed;  // 現在値
-double next_speed;  // 指令値
+double target_speed = 0;  // 目標値 m/s
+double current_speed = 0;  // 現在値 m/s
+double command_v = 0;  // 指令値 (pwm μs)
 
 //エンコーダ
 int en_count = 0;
@@ -36,9 +38,9 @@ int d_en_count = 0;
 
 //PID
 double p, i, d;
-const double Kp = 1.0, Ki = 0, Kd = 0;
-double diff[2];
-static double integral = 0;
+double Kp = 1, Ki = 0, Kd = 0;
+double d_speed[2]; // [0]が1ステップ前、[1]が最新
+double integral = 0;
 
 //エンコーダ
 void encoder(void) {
@@ -49,36 +51,27 @@ void encoder(void) {
   current_speed = d_en_count * 1000000 / SUMPLING_TIME_US / resolution * 2 * pi * r;
 }
 
-void limit(double &a){
-  if(a >= PERIOD * 0.8) a = PERIOD * 0.8;
-  else if(a　<=　-1　*　PERIOD　*　0.8) a　=　-1　*　PERIOD　*　0.8;
-}
-
 //PID
 void PID(void) {
-  diff[0] = diff[1];
-  diff[1] = target_speed - current_speed;
-  integral += (diff[1] + diff[0]) / 2.0 * (SUMPLING_TIME_US / (double)1000000);
-  p = Kp * diff[1];
+  d_speed[0] = d_speed[1];
+  d_speed[1] = target_speed - current_speed;
+  integral += (d_speed[1] + d_speed[0]) / 2.0 * (SUMPLING_TIME_US / (double)1000000);
+  p = Kp * d_speed[1];
   i = Ki * integral;
-  d = Kd * (diff[0] - diff[1]) / (SUMPLING_TIME_US　/　(double)1000000);
-  v += p + i + d;  // エンコーダーの差分から車輪の差分に変換し、足す
-  limit(v);
+  d = Kd * (d_speed[0] - d_speed[1]) / (SUMPLING_TIME_US　/　(double)1000000);
+  command_v += p + i + d;
+  motor.speed(command_v);
 }
 
-void motor_run() {
-  motor.speed(v);
-}
-
-void receive_speed() {
+void COMM(void) {
   target_speed = Ms.getfloat[0];
+  Ms.float_write(current_speed, 1);
 }
 
 int main() {
   while(switch1);
   enA.rise(encoder);
-  ticker1.attach_us(&speed_calc, SUMPLING_TIME_US);
-  ticker2.attach_us(&PID, SUMPLING_TIME_US);
-  ticker3.attach_us(&motor_run, SUMPLING_TIME_US);
-  ticker4.attach_us(&receive_speed, SUMPLING_TIME_US);
+  ticker1.attach_us(&PID, SUMPLING_TIME_US);
+  ticker2.attach_us(&COMM, COMM_TIME_US);
+  while(1) wait(1);
 }
--- mecanum_motor/src/motordriver.cpp
+++ mecanum_motor/src/motordriver.cpp
@@ -4,23 +4,23 @@
 #include "mbed.h"
 #include "PID.h"
 
-Motor::Motor(PwmOut &pwm, DigitalOut &dir, double pulse, bool brakeable) : _pwm(pwm), _dir(dir), Pulse(pulse), Brakeable(brakeable)
+Motor::Motor(PwmOut &pwm, DigitalOut &dir, double period, bool brakeable) : _pwm(pwm), _dir(dir), Period(period), Brakeable(brakeable)
 {
     // Set initial condition of PWM
-    _pwm.period_us(pulse);
+    _pwm.period_us(period);
     _pwm.pulsewidth_us(0);
 }
 
-void Motor::speed(double speed)
+void Motor::speed(double ＆speed)
 {
     if (speed > 0){
-        if(speed>=Pulse*0.50) speed=Pulse*0.50;//セッター
-        _dir=1;
+        if(speed >= Period*0.50) speed = Period*0.50;
+        _dir = 1;
         _pwm.pulsewidth_us(speed);
     }
     else if(speed < 0){
-        if(speed<=-1.0*Pulse*0.50) speed=-1*Pulse*0.50;//セッター
-        _dir=0;
+        if(speed <= -1.0*Period*0.50) speed = -1*Period*0.50;
+        _dir = 0;
         _pwm.pulsewidth_us(-1*speed);
     }
     else{
