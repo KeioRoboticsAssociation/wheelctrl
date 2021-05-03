aea8524e188deb2647b155d43c27eaf283525e2f
Naotchi <n.shiba0101@gmail.com>
2021-05-04 01:41:08 +0900

    add integral/duty limit

--- mecanum_motor/include/param.h
+++ mecanum_motor/include/param.h
@@ -1,5 +1,5 @@
-#ifndef __MAIN_H__
-#define __MAIN_H__
+#ifndef __PARAM_H__
+#define __PARAM_H__
 
 #define PI 3.14159265359
 #define SUMPLING_TIME_US 1000  // タイマー割り込み周期 kHzオーダーつまり1000が理想
@@ -7,5 +7,7 @@
 #define PERIOD 1000  // PWM制御のパルス幅 μs
 #define RESOLUTION 10  // エンコーダーの分解能
 #define TIRE_R  0.1  // タイヤの半径 m
+#define PWM_LIMIT 50
+#define INTEGRAL_LIMIT 100  // PID制御のIの値の発散防止
 
 #endif
