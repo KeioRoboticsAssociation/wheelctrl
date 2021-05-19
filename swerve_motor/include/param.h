40f51fb4056883a46b4aa9bbf8a2a8f4f4e7e1cd
Decwest <fumiyaonishi1016@gmail.com>
2021-05-19 14:49:13 +0900

    FEAT: easily change each motor pin config

--- swerve_motor/include/param.h
+++ swerve_motor/include/param.h
@@ -11,7 +11,7 @@
 #define PWM_LIMIT_TABLE 0.30
 #define INTEGRAL_LIMIT 1.0  // PID制御のIの値の発散防止
 
-#define Kp_WHEEL 0.5
+#define Kp_WHEEL 0.50
 #define Ki_WHEEL 0.0
 #define Kd_WHEEL 0.0
 
@@ -19,38 +19,74 @@
 #define Ki_TABLE 0.0
 #define Kd_TABLE 0.0
 
-#define STYLE_RF
+#define STYLE_LF
 
-#ifdef STYLE_RF
+#ifdef STYLE_RB
 
 #define PIN_GATE_DRIVER_ENABLE_WHEEL PB_5
-#define PIN_PWM_P_WHEEL PA_10
-#define PIN_PWM_N_WHEEL PA_11_ALT0
+#define PIN_PWM_P_WHEEL PA_11_ALT0
+#define PIN_PWM_N_WHEEL PA_10
 
 #define PIN_GATE_DRIVER_ENABLE_TABLE PB_4
 #define PIN_PWM_P_TABLE PB_0_ALT0
 #define PIN_PWM_N_TABLE PB_1_ALT0
 
-#define PIN_ENA_WHEEL PA_9
-#define PIN_ENB_WHEEL PA_8
+#define PIN_ENA_WHEEL PA_8
+#define PIN_ENB_WHEEL PA_9
 
 #define PIN_ENA_TABLE PA_6
 #define PIN_ENB_TABLE PA_7
 
 #endif
 
-#ifdef STYLE_HOGE
+#ifdef STYLE_RF
 
 #define PIN_GATE_DRIVER_ENABLE_WHEEL PB_5
-#define PIN_PWM_P_WHEEL PA_10
-#define PIN_PWM_N_WHEEL PA_11_ALT0
+#define PIN_PWM_P_WHEEL PA_11_ALT0
+#define PIN_PWM_N_WHEEL PA_10
 
 #define PIN_GATE_DRIVER_ENABLE_TABLE PB_4
 #define PIN_PWM_P_TABLE PB_0_ALT0
 #define PIN_PWM_N_TABLE PB_1_ALT0
 
-#define PIN_ENA_WHEEL PA_9
-#define PIN_ENB_WHEEL PA_8
+#define PIN_ENA_WHEEL PA_8
+#define PIN_ENB_WHEEL PA_9
+
+#define PIN_ENA_TABLE PA_6
+#define PIN_ENB_TABLE PA_7
+
+#endif
+
+#ifdef STYLE_LF
+
+#define PIN_GATE_DRIVER_ENABLE_WHEEL PB_5
+#define PIN_PWM_P_WHEEL PA_11_ALT0
+#define PIN_PWM_N_WHEEL PA_10
+
+#define PIN_GATE_DRIVER_ENABLE_TABLE PB_4
+#define PIN_PWM_P_TABLE PB_0_ALT0
+#define PIN_PWM_N_TABLE PB_1_ALT0
+
+#define PIN_ENA_WHEEL PA_8
+#define PIN_ENB_WHEEL PA_9
+
+#define PIN_ENA_TABLE PA_6
+#define PIN_ENB_TABLE PA_7
+
+#endif
+
+#ifdef STYLE_LB
+
+#define PIN_GATE_DRIVER_ENABLE_WHEEL PB_5
+#define PIN_PWM_P_WHEEL PA_11_ALT0
+#define PIN_PWM_N_WHEEL PA_10
+
+#define PIN_GATE_DRIVER_ENABLE_TABLE PB_4
+#define PIN_PWM_P_TABLE PB_1_ALT0
+#define PIN_PWM_N_TABLE PB_0_ALT0
+
+#define PIN_ENA_WHEEL PA_8
+#define PIN_ENB_WHEEL PA_9
 
 #define PIN_ENA_TABLE PA_6
 #define PIN_ENB_TABLE PA_7
